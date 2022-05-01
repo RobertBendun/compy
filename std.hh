f0359897dc7385a2f947097db57e83b2da6a4f6d
RobertBendun <robert@bendun.cc>
2022-05-02 01:08:18 +0200

    negative indexing

--- std.hh
+++ std.hh
@@ -5,36 +5,10 @@
 #include <string>
 #include <unordered_map>
 #include <variant>
+#include <vector>
 
 namespace python
 {
-	struct None {} None;
-	using Value_Variant = std::variant<struct None, bool, int, std::string>;
-
-	struct Value : Value_Variant
-	{
-		Value() : Value_Variant{None} {}
-		Value(struct None) : Value() {}
-		Value(bool b) : Value_Variant{b} {}
-		Value(std::string str) : Value_Variant{std::move(str)} {}
-
-		bool is_none() const
-		{
-			return std::holds_alternative<struct None>(*this);
-		}
-	};
-
-	struct Keyword_Arguments
-	{
-		std::unordered_map<std::string, Value> dict;
-
-		inline Keyword_Arguments& append(std::string const& key, Value value)
-		{
-			dict[key] = std::move(value);
-			return *this;
-		}
-	};
-
 	struct Error
 	{
 		std::string_view type;
@@ -52,6 +26,77 @@ namespace python
 	};
 
 	Error type_error{"TypeError"};
+}
+
+namespace python
+{
+	struct Value;
+	struct List;
+
+	struct None {} None;
+	using Bool = bool;
+	using Int = int;
+	using Str = std::string;
+
+	using Value_Variant = std::variant<struct None, Bool, Int, Str, List>;
+
+	struct List : std::vector<Value>
+	{
+		template<typename ...T>
+		static List init(T&& ...args)
+		{
+			if constexpr (0 == sizeof...(args)) {
+				return List{};
+			} else {
+				return List{ std::vector<Value>{{ Value(std::forward<T>(args))... }} };
+			}
+		}
+
+		auto& parent() { return *static_cast<std::vector<Value>*>(this); }
+
+		Value& operator[](int i)
+		{
+			assert(size_t(std::abs(i)) < size());
+			return i >= 0 ? parent()[i] : parent()[size() + i];
+		}
+
+		void append(Value &&value)
+		{
+			push_back(std::move(value));
+		}
+	};
+
+	struct Value : Value_Variant
+	{
+		Value() : Value_Variant{None} {}
+
+		template<typename T>
+		Value(T &&val) : Value_Variant(std::forward<T>(val)) {}
+
+		bool is_none() const
+		{
+			return std::holds_alternative<struct None>(*this);
+		}
+
+		Value& operator[](int i)
+		{
+			if (List* p = std::get_if<List>(this); p) {
+				return (*p)[i];
+			}
+			throw type_error("Subscript is only allowed for list types");
+		}
+	};
+
+	struct Keyword_Arguments
+	{
+		std::unordered_map<std::string, Value> dict;
+
+		inline Keyword_Arguments& append(std::string const& key, Value value)
+		{
+			dict[key] = std::move(value);
+			return *this;
+		}
+	};
 
 	template<typename T>
 	T assert_type(Value const& value, auto const& message)
@@ -81,6 +126,57 @@ namespace python
 	{
 		return type_or_none_default<T>(value, {}, message);
 	}
+}
+
+
+using list = python::List;
+using any = python::Value;
+
+list operator*(list l, int n)
+{
+	list result;
+
+	while (n-- > 0) {
+		result.insert(result.end(), l.begin(), l.end());
+	}
+
+	return result;
+}
+
+std::ostream& operator<<(std::ostream& os, any const& val);
+
+std::ostream& operator<<(std::ostream& os, list const& p)
+{
+	os << '[';
+
+	for (auto it = p.begin(); it != p.end(); ++it) {
+		os << *it;
+		if (std::next(it) != p.end())
+			os << ", ";
+	}
+
+	return os << ']';
+}
+
+std::ostream& operator<<(std::ostream& os, any const& val)
+{
+	if (std::holds_alternative<struct python::None>(val)) {
+		return os << "None";
+	}
+
+	if (auto p = std::get_if<bool>(&val); p) {
+		return os << (*p ? "True" : "False");
+	}
+
+	if (auto p = std::get_if<int>(&val); p) {
+		return os << *p;
+	}
+
+	if (auto p = std::get_if<list>(&val); p) {
+		return os << *p;
+	}
+
+	assert(false && "printing is not supported yet for this type");
 }
 
 std::string operator"" _str(char const* str, unsigned long length)
@@ -176,4 +272,19 @@ struct Range
 Range range(int from, int to)
 {
 	return { from, to };
+}
+
+Range range(int to)
+{
+	return { 0, to };
+}
+
+int len(any const& val)
+{
+	return python::assert_type<python::List>(val, "len() expects iterable object").size();
+}
+
+int len(list const& val)
+{
+	return val.size();
 }
